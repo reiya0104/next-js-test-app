9428e2a44ccdb8a3e8fae310834d3e2d28da541f
reiya0104 <reiya0104@ezweb.ne.jp>
2021-12-11 15:09:33 +0900

    Add Layout Component

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,17 +1,19 @@
-import { NextPage } from 'next'
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
-const FirstPost: NextPage = () => {
+export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
-
-export default FirstPost
